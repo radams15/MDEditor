d6b133e28d9da4fb9b67880f432016291896cf29
Rhys Adams (Home) <rhys@therhys.co.uk>
2022-04-14 16:08:30 +0100

    Moves windows ie compatability to ie11

--- src/main/MainApp.cpp
+++ src/main/MainApp.cpp
@@ -5,8 +5,18 @@
 #include "MainApp.h"
 #include "MainFrameCust.h"
 
+#ifdef __WXMSW__
+#include <wx/msw/registry.h>
+#endif
+
 bool MainApp::OnInit() {
     wxInitAllImageHandlers();
+
+#ifdef __WXMSW__
+    wxLogNull nolog;   // suppress error messages
+    wxRegKey rk( wxT("HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION") );
+    rk.SetValue( wxT("MDeditor.exe"), 11001);
+#endif
 
     MainFrame *frame = new MainFrameCust();
     frame->Show(true);
